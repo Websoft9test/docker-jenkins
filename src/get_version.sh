a4604af467ec9ad378ef7f078a7df852c317b11a
qiaofeng1227 <76487013@qq.com>
2022-07-20 14:15:10 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo jenkins version $(cat /data/wwwroot/jenkins/config.xml |grep version |sed -n 2p |tr -d "</>version") |sudo tee -a /data/logs/install_version.txt
+sudo echo jenkins version: $(docker exec -i $1 cat /data/wwwroot/jenkins/config.xml |grep version |sed -n 2p |tr -d "</>version") |sudo tee -a /data/logs/install_version.txt
