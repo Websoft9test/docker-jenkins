6e36163f27262bf9001c7775d6efd08a5b69874b
qiaofeng1227 <76487013@qq.com>
2022-10-17 14:27:22 +0800

    Update after_up.sh

--- src/after_up.sh
+++ src/after_up.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+clear
 
+sleep 30s
+echo "APP_PASSWORD=$1" >> /data/apps/jenkins/.env
+echo "APP_PASSWORD=$1" >> /credentials/password.txt
