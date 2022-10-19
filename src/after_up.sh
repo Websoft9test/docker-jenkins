399cc9e0c4606af5c491a71d16545c02c7f3c16a
qiaofeng1227 <76487013@qq.com>
2022-10-19 08:57:22 +0800

    Update after_up.sh

--- src/after_up.sh
+++ src/after_up.sh
@@ -4,7 +4,7 @@ echo "get password ..." >> /tmp/init_debug.txt
 while [  ! -f /data/apps/jenkins/data/jenkins/secrets/initialAdminPassword ]
 do
     sleep 3s
-    echo "initing..." >> /tmp/init_debug.txt
+    echo "initing,please wait ..." >> /tmp/init_debug.txt
 done
 jenkins_pwd=$(sudo cat /data/apps/jenkins/data/jenkins/secrets/initialAdminPassword)
 echo "APP_PASSWORD=$jenkins_pwd" >> /data/apps/jenkins/.env
