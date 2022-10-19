4b990be0dcb4670ce79c800b338ec5c98b88c8bc
qiaofeng1227 <76487013@qq.com>
2022-10-19 08:45:03 +0800

    Update after_up.sh

--- src/after_up.sh
+++ src/after_up.sh
@@ -2,12 +2,10 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 clear
 
-jenkins_pwd=$(sudo docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
-echo "# get password for docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword">> /data/apps/jenkins/.env
-while [  ! -n "$jenkins_pwd" ]
+while [  ! -f /data/apps/jenkins/data/jenkins/secrets/initialAdminPassword ]
 do
     sleep 3s
-    jenkins_pwd=$(sudo docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
+    echo "initing..." >> /tmp/init_debug.txt
 done
+jenkins_pwd=$(sudo docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
 echo "APP_PASSWORD=$jenkins_pwd" >> /data/apps/jenkins/.env
-
